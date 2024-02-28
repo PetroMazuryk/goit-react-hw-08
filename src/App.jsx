712649b8cf82807f5c4c1c0d7fbd38b22e909286
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-28 23:29:10 +0200

    added refresh user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout.jsx';
 import { RestrictedRoute } from './components/RestrictedRoute.jsx';
 import { PrivateRoute } from './components/PrivateRoute.jsx';
+import { useAuth } from './hooks/useAuth.js';
+import { refreshUser } from './redux/auth/operations.js';
 
 import Home from './pages/Home/Home.jsx';
 import Register from './pages/Register/Register.jsx';
@@ -9,7 +13,15 @@ import Login from './pages/Login/Login.jsx';
 import Contacts from './pages/Contacts/Contacts.jsx';
 
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { App } from './App.jsx';
 import 'modern-normalize';
@@ -10,9 +11,11 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
