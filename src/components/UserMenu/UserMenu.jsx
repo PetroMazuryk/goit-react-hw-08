a3c9c7f5b3cd6f8c2685dbf1b4142c5c36d4311e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-28 23:11:03 +0200

    added logout

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,13 +1,22 @@
+import { useDispatch } from 'react-redux';
 import { useAuth } from '../../hooks';
+import { logOut } from '../../redux/auth/operations';
 import css from './UserMenu.module.css';
 
 export const UserMenu = () => {
+  const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome, {user.name}</p>
-      <button type="button">Logout</button>
+      <button
+        className={css.button}
+        type="button"
+        onClick={() => dispatch(logOut())}
+      >
+        Logout
+      </button>
     </div>
   );
 };
