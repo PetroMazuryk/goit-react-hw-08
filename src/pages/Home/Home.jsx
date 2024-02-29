76c72f99090c2b952e5d99b7d84bbb12ccec1782
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-29 18:44:21 +0200

    fixed page Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import { HomeTitle } from '../../components/HomeTitle/HomeTitle';
 import { PhoneTitle } from '../../components/PhoneTitle/PhoneTitle';
 
 export default function Home() {
   return (
     <div>
       <PhoneTitle />
+      <HomeTitle />
     </div>
   );
 }
