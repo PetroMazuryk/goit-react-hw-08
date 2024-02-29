3dc9c15f5b95510a062d5bc51459141f5353bc1d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-29 18:02:08 +0200

    import

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,7 +2,7 @@ import { FcTabletAndroid } from 'react-icons/fc';
 import { IconContext } from 'react-icons';
 import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
-import { AuthNav } from '../AuthNav/AuthNav';
+import { AuthNav } from '../../components/AuthNav/AuthNav';
 import { useAuth } from '../../hooks';
 
 import css from './AppBar.module.css';
