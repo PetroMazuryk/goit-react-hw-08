5c7fb1c8135bcf8a8cd08cdd1af5548dbc073d68
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-02-29 17:45:53 +0200

    fixed page Home

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route
-            path="register"
+            path="/register"
             element={
               <RestrictedRoute
                 redirectTo="/contacts"
@@ -37,13 +37,13 @@ export const App = () => {
             }
           />
           <Route
-            path="login"
+            path="/login"
             element={
               <RestrictedRoute redirectTo="/contacts" component={<Login />} />
             }
           />
           <Route
-            path="contacts"
+            path="/contacts"
             element={
               <PrivateRoute redirectTo="/login" component={<Contacts />} />
             }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import { PhoneTitle } from '../../components/PhoneTitle/PhoneTitle';
-import { HomeTitle } from '../../components/HomeTitle/HomeTitle';
 
 export default function Home() {
   return (
     <div>
       <PhoneTitle />
-      <HomeTitle />
     </div>
   );
 }
